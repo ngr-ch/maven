7fae1bbc490a878684de1bc101cd6eff4b554d85
ngr-ch <111493473+ngr-ch@users.noreply.github.com>
2025-02-18 10:33:14 +0530

    Update App.java

--- src/main/java/ToolsQA/App.java
+++ src/main/java/ToolsQA/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello Wwe are Migrating out App into Azure App Service" );
     }
 }
